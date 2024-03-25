35f1dfae885b0b2d1c3ebeb968f156b7ccfd830d
sne13978 <sneha.banerjee@ipa.fraunhofer.de>
2024-03-25 15:15:32 +0100

    dbcv as a separate method in parse

--- calculate_dbcv.py
+++ calculate_dbcv.py
@@ -3,26 +3,26 @@ import numpy as np
 import torch
 torch.cuda.is_available()
 from tqdm import tqdm
-from utils.parse import parse_args, load_model, get_clusters, get_pca, get_tsne
+from utils.parse import parse_args, load_model, get_clusters, get_pca, get_tsne, calc_dbcv
 from dataset.dataloader import data_loader
 from DBCV.DBCV_multiproc import DBCV
 # from DBCV.DBCV_neighbor import DBCV
 # from DBCV.DBCV import DBCV
 
 config = parse_args()
-test_data_loader = data_loader(config)
+train_data_loader, val_data_loader = data_loader(config)
 with torch.cuda.device(config['util']['gpu']):
     ema_net = load_model(config)
     all_points = []
     start = time.time()
-    for batch_id, test_data in tqdm(enumerate(test_data_loader, 0), total=len(test_data_loader), smoothing=0.9,desc = 'test dataset'):
-        x1_test = test_data[0]
-        x1_test = x1_test.cuda()
-        x1_test = x1_test.transpose(2, 1)
-        emb =  ema_net(x1_test,return_embedding=config['training']['return_embedding'])
+    for batch_id, train_data in tqdm(enumerate(train_data_loader, 0), total=len(train_data_loader), smoothing=0.9,desc = 'train dataset'):
+        x1_train = train_data[0]
+        x1_train = x1_train.cuda()
+        x1_train = x1_train.transpose(2, 1)
+        emb =  ema_net(x1_train,return_embedding=config['training']['return_embedding'])
         all_points.append(emb[0].detach().cpu().numpy())
     del ema_net
-    del test_data_loader
+    del train_data_loader
     all_points = np.concatenate(all_points,axis=0)
     end = time.time()
     np.random.seed(42)
@@ -38,9 +38,5 @@ with torch.cuda.device(config['util']['gpu']):
 
     clf, pred, num_clusters = get_clusters(data=all_points,store_centers='medoid',classifier=config['results']['classifier'],min_samples=2,n_clusters=config['results']['n_clusters'],batch_size=config['results']['chunk_size'])
     
-    start = time.time()
-    dbcv_score = DBCV(all_points,pred)
-    print(f"DBCV score is {dbcv_score}")
-    end = time.time()
-    print(f'DBCV took {end-start} seconds or {(end-start)/60} minutes or {(end-start)/3600} hours for {all_points.shape[0]} datapoints')
+    dbcv_score = calc_dbcv(all_points,pred)
         
