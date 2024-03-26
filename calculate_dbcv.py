8ecfc8e59100be90145bb804e80194c064980804
sne13978 <sneha.banerjee@ipa.fraunhofer.de>
2024-03-26 14:29:50 +0100

    no need for two separate files

--- calculate_dbcv.py
+++ calculate_dbcv.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 torch.cuda.is_available()
 from tqdm import tqdm
-from utils.parse import parse_args, load_model, get_clusters, get_pca, get_tsne, calc_dbcv
+from utils.parse import parse_args, load_model, get_clusters, get_pca, get_tsne, calc_dbcv, get_hier_clusters
 from dataset.dataloader import data_loader
 from DBCV.DBCV_multiproc import DBCV
 # from DBCV.DBCV_neighbor import DBCV
@@ -39,4 +39,26 @@ with torch.cuda.device(config['util']['gpu']):
     clf, pred, num_clusters = get_clusters(data=all_points,store_centers='medoid',classifier=config['results']['classifier'],min_samples=2,n_clusters=config['results']['n_clusters'],batch_size=config['results']['chunk_size'])
     
     dbcv_score = calc_dbcv(all_points,pred)
-        
+
+    if config['results']['classifier'] == 'hdbscan':
+        rel_points = all_points.copy()
+        original_clusters = pred.copy()
+        cluster_members = []
+        clusters = np.unique(original_clusters).tolist()
+        for cluster in clusters:
+            idx = (original_clusters == cluster).nonzero()[0]
+            cluster_points = np.take(all_points,idx,axis=0)
+            cluster_members.append(cluster_points)
+
+        count=0
+
+        while True:
+            rel_points, original_clusters, clusters, cluster_members, num_clusters, clf = get_hier_clusters(clf = clf, all_points=all_points,rel_points=rel_points,  original_clusters=original_clusters, clusters = clusters, cluster_members=cluster_members, min_samples=config['results']['min_samples'], classifier=config['results']['classifier'],count=count)
+
+            if count!=0:
+                dbcv_score = calc_dbcv(all_points,original_clusters)
+            count+=1
+
+            if num_clusters<=config['results']['n_clusters']:
+                break
+            
